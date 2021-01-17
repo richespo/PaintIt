00ad6fb23ed0788e1d3d8a19e28f3aedf9a13cff
rich <richespo@mac.com>
2021-01-17 11:17:16 -0800

    working on file list actions

--- paintIt.py
+++ paintIt.py
@@ -9,16 +9,16 @@ screen_x, screen_y = 640, 480
 
 class Ui_MainWindow(QObject):
     def setupUi(self, MainWindow):
-        # sizeObject = QDesktopWidget().screenGeometry(-1)
-        # screen_y, screen_x = sizeObject.height(), sizeObject.width()
+        sizeObject = QDesktopWidget().screenGeometry(-1)
+        self.screen_y, self.screen_x = sizeObject.height(), sizeObject.width()
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(screen_x, screen_y)
+        MainWindow.resize(self.screen_x, self.screen_y)
         #     MainWindow.setMaximumSize(QtCore.QSize(screen_x, screen_y))
         #     MainWindow.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(0, 0, screen_x, screen_y))
+        self.graphicsView.setGeometry(QtCore.QRect(0, 0, self.screen_x, self.screen_y))
         self.graphicsView.setObjectName("graphicsView")
         self.graphicsView.setHorizontalScrollBarPolicy(1);
         self.graphicsView.setVerticalScrollBarPolicy(1);
@@ -36,7 +36,7 @@ class Ui_MainWindow(QObject):
         self.actionOpen.setObjectName("actionOpen")
         self.menuOpen.addAction(self.actionOpen)
         self.menubar.addAction(self.menuOpen.menuAction())
-        self.actionOpen.triggered.connect(theMaster.getFileList)
+        self.actionOpen.triggered.connect(theMaster.mastering)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -57,17 +57,23 @@ class Master():
         self.screen_y = screenGeometry.height()
         self.screen_x = screenGeometry.width()
 
-    def getFileList(self):
-        fdialog = QFileDialog()
-        fdialog.setFileMode(QFileDialog.DirectoryOnly)
-        file_name = QFileDialog.getExistingDirectory(None, 'Choose Directory', "/Users/rich")
-        image_list = [os.path.join(file_name, f) for f in os.listdir(file_name) if f.endswith('.jpg')]
-     #   for img in image_list:
-            # self.next_image = QImage(img)
-            # resized_image = self.scaleImage(self.next_image)
-        transitioner.initTransition()
-        transitioner.transition()
+    def mastering(self):
+        image_list = self.getFileList()
+        transitioner.pix = QPixmap(theMaster.screen_x, theMaster.screen_y)
+        transitioner.painter.begin(transitioner.pix)
+        for img in image_list:
+            newImage = self.scaleImage(QImage(img))
+            transitioner.initTransition(newImage)
+            transitioner.transition()
 
+
+    def getFileList(self):
+        # fdialog = QFileDialog()
+        # fdialog.setFileMode(QFileDialog.DirectoryOnly)
+        # file_name = QFileDialog.getExistingDirectory(None, 'Choose Directory', "/Users/rich")
+        file_name = "/Users/rich/smallFrame"
+        image_list = [os.path.join(file_name, f) for f in os.listdir(file_name) if f.endswith('.jpg')]
+        return image_list
 
 
     def scaleImage(self, im):
@@ -77,8 +83,18 @@ class Master():
             scaledImage = im.scaledToHeight(self.screen_y, QtCore.Qt.FastTransformation)
             if scaledImage.width() > self.screen_x:                                               # width needs to be cropped
                 increment = (scaledImage.width() - self.screen_x) / 2                             # crop 1/2 oversize from each side
-                scaledImage = scaledImage.copy(int(increment), 0, int(scaledImage.width() - increment), int(self.screen_y))
-            return  scaledImage
+                scaledImage = scaledImage.copy(increment, 0, int(scaledImage.width() - (2*increment)), int(self.screen_y))
+                return scaledImage
+            elif scaledImage.width() < self.screen_x:
+                increment = (self.screen_x - scaledImage.width()) / 2
+                blkbox = QImage("blackImage.jpg")
+                myPainter = QPainter(blkbox)
+                dest = QPoint(increment,0)
+                myPainter.begin(blkbox)
+                myPainter.drawImage(dest, scaledImage)
+                myPainter.end()
+                return  blkbox
+
 
 class TransitionMaster():
 
@@ -91,20 +107,19 @@ class TransitionMaster():
         self.scene = QGraphicsScene()
         self.numSlices = 9
 
-    def initTransition(self):
-        self.newImage = QImage("DSC_0035.jpg")
+    def initTransition(self, im):
+        self.newImage = im
         self.slice = 0
         # self.transition_type = random.randint(0,6)
         self.transition_type = 4
-        self.displayImage = QImage("cam92_1.jpg")
-        self.pix = QPixmap(self.displayImage)
-        self.painter.begin(self.pix)
+   #     self.displayImage = QImage("DSC_0035.jpg")
+
 
     def transition(self):
         self.slice += 1
         self.theTransition()
         if self.slice > self.numSlices:
-            self.displayImage = self.newImage
+            self.displayImage = self.pix.toImage()
         else:
             item = QGraphicsPixmapItem(self.pix)
             self.scene.addItem(item)
@@ -129,9 +144,10 @@ class TransitionMaster():
             self.painter.drawImage(dest_point, cropped)
         elif self.transition_type == 4:                 #wipe up
             self.chunk = int((self.newImage.height() * self.slice) / self.numSlices)
-            cropped = self.newImage.copy(0, self.newImage.height()-self.chunk, self.newImage.width(), self.newImage.height())
+            self.cropped = self.newImage.copy(0, self.newImage.height()-self.chunk, self.newImage.width(), self.newImage.height())
             dest_point = QPoint(0,self.newImage.height()-self.chunk)
-            self.painter.drawImage(dest_point, cropped)
+            self.painter.drawImage(dest_point, self.cropped)
+        #    self.painter.end()
 
 
 
